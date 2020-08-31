639e021a8c10f5e2ff2880b02d4d088923ab8a87
Robert Franke <robert.franke@nirx.de>
2020-08-31 17:53:25 +0200

    Simplify nim version

--- layer1.nim
+++ layer1.nim
@@ -1,36 +1,25 @@
 import strutils
 import bitops
 
-proc decodeGroup(group: string): string =
-    var res : string = ""
-    var total: uint32 = 0
-    for i in 0..4:
-        total = 85 * total + (uint32(ord(group[i])) - 33)
-    res.add(char((total shr 24) and 255))
-    res.add(char((total shr 16) and 255))
-    res.add(char((total shr 8) and 255))
-    res.add(char((total) and 255))
-    return res
-
 proc ascii85decode(b: string): string =
-    var i: int = 0
     var res: string = ""
-    var decoded: string
-    while i < len(b):
-        if b[i] == 'z':
-            result.add("\0\0\0\0")
-            i += 1
+    var padded: string = b & "uuuu"
+    var count: int = 0
+    var total: uint32 = 0
+    for c in padded.items:
+        if c == 'z':
+            res.add("\0\0\0\0")
             continue
-        if i + 5 < len(b):
-            decoded = decodeGroup(b[i..i+4])
-        else:
-            var padded: string = b[i..len(b)-1]
-            var padding: int = 5 - len(padded)
-            padded.add("u".repeat(padding))
-            decoded = decodeGroup(padded)[0..3-padding]
-        res.add(decoded)
-        i += 5
-    return res
+        total = 85 * total + (uint32(ord(c) - 33))
+        count += 1
+        if count == 5:
+            res.add(char((total shr 24) and 255))
+            res.add(char((total shr 16) and 255))
+            res.add(char((total shr 8) and 255))
+            res.add(char((total) and 255))
+            total = 0
+            count = 0
+    return res[0..^(5 - count)]
 
 proc main() =
     let f = open("payload_layer1.txt")
