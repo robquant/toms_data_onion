ce21600317ec69143f5136085620039f67665e20
Robert Franke <franke.rob@gmail.com>
2020-09-04 17:41:50 +0200

    WIP

--- layer6.cr
+++ layer6.cr
@@ -26,10 +26,34 @@ def a85decode(encoded : String) : Array(UInt8)
     return result
 end
 
-class Program
-    def new()
+class Tomtel69CPU
+    ptr : UInt32
+    pc : UInt32
+    def initialize(program : Array(UInt8))
+        @memory = program
+        @reg8 = {:a => 0, :b => 0, :c => 0, :d => 0, :e => 0, :f => 0} of Symbol => UInt8
+        @reg32 = {:la => 0, :lb => 0, :lc => 0, :ld => 0} of Symbol => UInt32
+        @ptr = 0
+        @pc = 0
+    end
+
+    def run()
+        while true
+            case @memory[@pc]
+            when 0xC2
+                puts "ADD"
+            when 0xE1
+                puts "APTR"
+                @pc += 1
+            when 0x01
+                break
+            end
+            @pc += 1
+        end
     end
 end
 
 encoded = File.read_lines("payload_layer6.txt").join("")[2...-2]
 decoded = a85decode(encoded)
+tomtel69 = Tomtel69CPU.new decoded
+tomtel69.run
